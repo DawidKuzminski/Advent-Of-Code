232b8a147d7cc398392a762910afe90381861d8b
Dawid Kuźmiński <d.kuzminski@outlook.com>
2022-12-09 16:31:57 +0100

    2022 - Day 1 completed

---
+++ 2021/Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿string path = @"D:\PROJECTS\Advent-Of-Code\2021\Day1\InputData.txt";
+
+if(File.Exists(path))
+{
+	var inputData = File.ReadAllText(path).Split(Environment.NewLine);
+	int count = 0;
+
+	for (int i = 1; i < inputData.Length; i++)
+	{
+		Int32.TryParse(inputData[i], out int currentValue);
+		Int32.TryParse(inputData[i - 1], out int previousValue);
+		
+		if(currentValue > previousValue)
+			count++;
+	}
+
+	Console.WriteLine($"{count} measurements are larger than the previous measurement");
+}
+
+
+
+int[] PrepareAdvancedInput(string[] inputData)
+{
+
+	for (int i = 1; i < inputData.Length; i++)
+	{
+		Int32.TryParse(inputData[i], out int currentValue);
+		Int32.TryParse(inputData[i - 1], out int previousValue);			
+	}
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,42 @@
+﻿//See https://aka.ms/new-console-template for more information
+
+using System.IO;
+
+Console.WriteLine("Hello, World!");
+
+
+var inputData = File.ReadAllLines(@"..\..\..\InputData.txt");
+
+var elvesInvetories = new Dictionary<int, int>();
+int caloriesCounter = 0;
+int elvsCounter = 1;
+
+foreach (var calories in inputData)
+{
+	if (calories != string.Empty)
+	{
+		if (int.TryParse(calories, out var caloriesAsNumber))
+		{ caloriesCounter += caloriesAsNumber; }
+	}
+	else
+	{
+		elvesInvetories.Add(elvsCounter, caloriesCounter);
+		caloriesCounter = 0;
+		elvsCounter++;
+	}
+}
+
+elvesInvetories = elvesInvetories.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+Console.WriteLine($"Elvs with most calories: {elvesInvetories.First().Key}. Number of calories: {elvesInvetories.First().Value}");
+Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+//Part 2
+
+var sumOfCaloriesTopThreeElves = 0;
+for(int i = 0; i < 3; i++)
+{
+	sumOfCaloriesTopThreeElves += elvesInvetories.ElementAt(i).Value;
+}
+
+Console.WriteLine($"Top 3 elves have {sumOfCaloriesTopThreeElves} calories in inventories");
+
+Console.ReadKey();
